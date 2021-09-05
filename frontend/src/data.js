d0891745675ec2b9affff76e9dade24e02ee5794
Haroun Ach <harounacho21@gmail.com>
2021-09-05 22:02:28 +0100

    Populate product cards with dummy data

---
+++ frontend/src/data.js
@@ -0,0 +1,83 @@
+const data = {
+  products: [
+    /* product 1 */
+    {
+      _id: "1",
+      name: "Nike Slim Shirt",
+      category: "Shirts",
+      image: "/images/p1.jpg",
+      price: 120,
+      brand: "Nike",
+      ratings: 4.5,
+      numReviews: 10,
+      description: "high quality product",
+    },
+
+    /* product 2 */
+    {
+      _id: "2",
+      name: "Adidas Fit Shirt",
+      category: "Shirts",
+      image: "/images/p2.jpg",
+      price: 100,
+      brand: "Adidas",
+      ratings: 4.0,
+      numReviews: 10,
+      description: "high quality product",
+    },
+
+    /* product 3 */
+    {
+      _id: "3",
+      name: "Lacoste Free Shirt",
+      category: "Shirts",
+      image: "/images/p3.jpg",
+      price: 120,
+      brand: "Lacoste",
+      ratings: 4.5,
+      numReviews: 17,
+      description: "high quality product",
+    },
+
+    /* product 4 */
+    {
+      _id: "4",
+      name: "Nike Slim Pant",
+      category: "Pants",
+      image: "/images/p4.jpg",
+      price: 78,
+      brand: "Nike",
+      ratings: 4.5,
+      numReviews: 40,
+      description: "high quality product",
+    },
+
+    /* product 5 */
+    {
+      _id: "5",
+      name: "Puma Slim Pant",
+      category: "Pants",
+      image: "/images/p5.jpg",
+      price: 65,
+      brand: "Puma",
+      ratings: 4.5,
+      numReviews: 10,
+      description: "high quality product",
+    },
+
+    /* product 6 */
+    {
+      _id: "6",
+      name: "Adidas Fit Pant",
+      category: "Pants",
+      image: "/images/p6.jpg",
+      price: 139,
+      brand: "Adidas",
+      ratings: 4.5,
+      numReviews: 15,
+      description: "high quality product",
+    },
+  ],
+};
+
+export default data;
