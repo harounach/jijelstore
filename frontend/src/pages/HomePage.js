3465434a1d11b87a2621b55cbabcfb735f8bc3dc
Haroun Ach <harounacho21@gmail.com>
2021-09-06 15:05:35 +0100

    Fetch data fro the server

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,13 +1,40 @@
-import React from "react";
-import data from "../data";
+import { useState, useEffect } from "react";
 import Product from "../components/Product";
+import axios from "axios";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
 
 export default function HomePage() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get("/api/products");
+        setLoading(false);
+        setProducts(data);
+      } catch (error) {
+        setError(error.message);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
   return (
-    <div className="row center">
-      {data.products.map((product) => {
-        return <Product key={product._id} product={product} />;
-      })}
+    <div>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger"> {error} </MessageBox>
+      ) : (
+        <div className="row center">
+          {products.map((product) => {
+            return <Product key={product._id} product={product} />;
+          })}
+        </div>
+      )}
     </div>
   );
 }
--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -48,7 +48,7 @@ export default function ProductPage(props) {
                     {product.countInStock > 0 ? (
                       <span className="success">In Stock</span>
                     ) : (
-                      <spa className="error">Unavailable</spa>
+                      <span className="danger">Unavailable</span>
                     )}
                   </div>
                 </div>
