a180776055d708590feec7a4a6a8518ba9100e2d
Haroun Ach <harounacho21@gmail.com>
2021-09-10 20:10:47 +0100

    Implement add to cart action

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,29 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
 import CartPage from "./pages/CartPage";
 
 function App() {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
   return (
     <BrowserRouter>
       <div className="grid-container">
         <header className="row">
           <div>
-            <a className="brand" href="/">
+            <Link className="brand" to="/">
               JijelStore
-            </a>
+            </Link>
           </div>
           <div>
-            <a href="/cart">Cart</a>
-            <a href="/signin">Sign In</a>
+            <Link to="/cart">
+              Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            <Link to="/signin">Sign In</Link>
           </div>
         </header>
         <main>
