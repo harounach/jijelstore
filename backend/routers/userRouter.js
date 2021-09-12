f98ef8370847cc25aa61726b8af224e56d61aebb
Haroun Ach <harounacho21@gmail.com>
2021-09-12 16:40:51 +0100

    Create sample users in mongodb

---
+++ backend/routers/userRouter.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const expressAsyncHandler = require("express-async-handler");
+const data = require("../data");
+const User = require("../models/userModel");
+
+const userRouter = express.Router();
+
+userRouter.get(
+  "/seed",
+  expressAsyncHandler(async (req, res) => {
+    await User.remove({});
+    const createdUsers = await User.insertMany(data.users);
+    res.send({ createdUsers });
+  })
+);
+
+module.exports = userRouter;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const app = express();
 
+const mongoose = require("mongoose");
+
 const data = require("./data");
+const userRouter = require("./routers/userRouter");
 
 const port = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGODB_URL || "mongodb://localhost/jijelstore", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    //useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 app.get("/", (req, res) => {
   res.send("Server is ready");
@@ -21,6 +37,8 @@ app.get("/api/products/:id", (req, res) => {
     res.status(404).send({ message: "Product Not Found" });
   }
 });
+
+app.use("/api/users", userRouter);
 
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
