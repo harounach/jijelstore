0f6541fd76927a6f040ba45dbbb3103860ba73fa
Haroun Ach <harounacho21@gmail.com>
2021-09-12 17:38:21 +0100

    Implement signin backend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,10 @@
+const dotenv = require("dotenv");
 const express = require("express");
+
+dotenv.config();
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const mongoose = require("mongoose");
 
