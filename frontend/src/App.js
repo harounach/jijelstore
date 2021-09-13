698e5a45db8894cbdfa00ad07bb6aa7fab43d877
Haroun Ach <harounacho21@gmail.com>
2021-09-13 11:31:39 +0100

    Design signin page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Link } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
 import CartPage from "./pages/CartPage";
+import SigninPage from "./pages/SigninPage";
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -29,6 +30,7 @@ function App() {
         <main>
           <Route path="/cart/:id?" component={CartPage}></Route>
           <Route path="/product/:id" component={ProductPage}></Route>
+          <Route path="/signin" component={SigninPage}></Route>
           <Route path="/" component={HomePage} exact></Route>
         </main>
         <footer className="row center">All right reserved</footer>
