f7615e1de2cd68396314a7c716a1d3dccbe09f30
Haroun Ach <harounacho21@gmail.com>
2021-09-13 16:31:16 +0100

    Create shipping page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import ProductPage from "./pages/ProductPage";
 import CartPage from "./pages/CartPage";
 import SigninPage from "./pages/SigninPage";
 import RegisterPage from "./pages/RegisterPage";
+import ShippingAddressPage from "./pages/ShippingAddressPage";
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -55,6 +56,7 @@ function App() {
           <Route path="/product/:id" component={ProductPage}></Route>
           <Route path="/signin" component={SigninPage}></Route>
           <Route path="/register" component={RegisterPage}></Route>
+          <Route path="/shipping" component={ShippingAddressPage}></Route>
           <Route path="/" component={HomePage} exact></Route>
         </main>
         <footer className="row center">All right reserved</footer>
